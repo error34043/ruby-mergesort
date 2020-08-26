e14c07afb9d1b43b1440b9af682c6006e3f390f0
Sanjana <sanjhemdev@gmail.com>
2020-08-27 04:42:25 +0530

    Final commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# ruby-mergesort
+# Mergesort
+
+This program takes an array of number and recursively sorts them in ascending order accoring to [the Mergesort algorithm](https://en.wikipedia.org/wiki/Merge_sort).
--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
 # frozen_string_literal: true
+
+def merge_sort(arr, result = [])
+  return arr if arr.length < 2
+
+  left_half = merge_sort(arr.slice(0, arr.length / 2))
+  right_half = merge_sort(arr.slice(arr.length / 2, arr.length))
+
+  until left_half.empty? || right_half.empty?
+    result.push(left_half[0] < right_half[0] ? left_half.shift : right_half.shift)
+  end
+
+  result.push(*left_half.push(*right_half))
+end
